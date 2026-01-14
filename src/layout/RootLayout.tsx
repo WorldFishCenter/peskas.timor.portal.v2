f05e358768fe7ba59e6a387eea475713f3e7652c
langbart <lorenzo.longobardi@gmail.com>
2026-01-14 20:19:53 +0100

    fix: Update header class for sticky navigation in RootLayout component

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout() {
   return (
     <div className="page">
       {/* Top header */}
-      <header className="navbar navbar-expand-md d-print-none">
+      <header className="navbar navbar-expand-md sticky-top d-print-none">
         <div className="container-xl">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-menu" aria-controls="navbar-menu" aria-expanded="false" aria-label={t('actions.toggle_navigation', { defaultValue: 'Toggle navigation' })}>
             <span className="navbar-toggler-icon"></span>
